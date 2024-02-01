0b89bee7f0bfb522c77653000a255948d5ae9615
akanoao <dakshgakhar18@gmail.com>
2024-02-01 20:35:04 +0530

    face recog ki class sahi kari hai

--- coding/webcam.py
+++ coding/webcam.py
@@ -2,18 +2,9 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOT: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-class face_recognition():
-    def  facerecognition(self):
-# Get a reference to webcam #0 (the default one)
+class facrecognition():
+    def facerecognition(self):
+    # Get a reference to webcam #0 (the default one)
         video_capture = cv2.VideoCapture(0)
 
         # Load a sample picture and learn how to recognize it.
@@ -94,13 +85,14 @@ class face_recognition():
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-    # cv2.waitKey(1)
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # Display the resulting image
+            cv2.imshow('Video', frame)
+            # cv2.waitKey(1)
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
         # Release handle to the webcam
         video_capture.release()
         cv2.destroyAllWindows()
+
