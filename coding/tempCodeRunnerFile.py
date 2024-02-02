fb9dff0078ef17813ac03a6e8f3d1ee469d0a732
akanoao <dakshgakhar18@gmail.com>
2024-02-03 02:38:21 +0530

    attendance sys

--- coding/tempCodeRunnerFile.py
+++ coding/tempCodeRunnerFile.py
@@ -1,4 +1,2 @@
-
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("indv1.jpeg")
-obama_face_encoding = face_recognition.face_encodings(
+1280)
+# video_capture.set(4,72
