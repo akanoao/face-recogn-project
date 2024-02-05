4a1492ecf8ac90f3c6aa4046fdd64e6408b1c6b7
anuj <anujg6263@gmail.com>
2024-02-06 02:12:17 +0530

    gui add

--- coding/security.py
+++ coding/security.py
@@ -8,8 +8,8 @@ import sqlite3
 class security():
     def window(self, notebook, window):
         frame = ttk.Frame(notebook, width=400, height=280)
-
-        scan_button = tk.Button(frame, text="Scan", command=lambda: [])
+        import weapon_detect
+        scan_button = tk.Button(frame, text="Scan", command=lambda: [weapon_detect.detect()])
         scan_button.pack(anchor=tk.CENTER, padx=5, pady=5)
 
         return frame
--- coding/weapon_detect.py
+++ coding/weapon_detect.py
@@ -5,45 +5,46 @@ import datetime
 from playsound import playsound
 import time
 
-gun_cascade = cv2.CascadeClassifier('cascade.xml')
-camera = cv2.VideoCapture(0)
-camera.set(3,1280)
-camera.set(4,720)
-firstFrame = None
-gun_exist = False
-while True:
-    ret, frame = camera.read()
-    if frame is None:
-        break
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gun = gun_cascade.detectMultiScale(gray, 1.3, 20, minSize=(100, 100))
-    if len(gun) > 0:
-        gun_exist = True
-        playsound('siren.mp3', block=False)
-        
-        # try:
-        #     playsound("siren.mp3",block="False")
-        #     time.sleep(2)
-        # except:
-        #     pass
-    for (x, y, w, h) in gun:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S %p"),
-    (10, frame.shape[0] - 10),cv2.FONT_HERSHEY_SIMPLEX,0.35, (0, 255, 255), 1)
-    
-    # print("Guns detected")
-    cv2.imshow("pic",frame)
-    # else:
-    #     cv2.imshow("Security Feed", frame)
-    # key = cv2.waitKey(1) & 0xFF
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def detect():
+    gun_cascade = cv2.CascadeClassifier('cascade.xml')
+    camera = cv2.VideoCapture(0)
+    camera.set(3,1280)
+    camera.set(4,720)
+    firstFrame = None
+    gun_exist = False
+    while True:
+        ret, frame = camera.read()
+        if frame is None:
+            break
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gun = gun_cascade.detectMultiScale(gray, 1.3, 20, minSize=(100, 100))
+        if len(gun) > 0:
+            gun_exist = True
+            playsound('siren.mp3', block=False)
 
-camera.release()
-cv2.destroyAllWindows()
+            # try:
+            #     playsound("siren.mp3",block="False")
+            #     time.sleep(2)
+            # except:
+            #     pass
+        for (x, y, w, h) in gun:
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+        if firstFrame is None:
+            firstFrame = gray
+            continue
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S %p"),
+        (10, frame.shape[0] - 10),cv2.FONT_HERSHEY_SIMPLEX,0.35, (0, 255, 255), 1)
+
+        # print("Guns detected")
+        cv2.imshow("pic",frame)
+        # else:
+        #     cv2.imshow("Security Feed", frame)
+        # key = cv2.waitKey(1) & 0xFF
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
